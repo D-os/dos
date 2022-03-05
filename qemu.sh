415e5f15bd786f21d49c4b3448d8279929c24dd1
Tomasz Sterna <tomasz@sterna.link>
2022-03-05 22:03:23 +0100

    Update qemu script to see kernel logs

--- qemu.sh
+++ qemu.sh
@@ -1,6 +1,9 @@
 cd $(dirname $0)/out
 IMG=tmp.img
 trap 'rm $IMG' 1 2 3 15
-dd if=/dev/zero of=$IMG bs=1024 count=1024
-qemu-system-x86_64 -append "root=/dev/sda console=ttyS0" -serial stdio -m 1G -hda $IMG -kernel build/linux/arch/x86/boot/bzImage #-initrd build/initrd/initrd
+qemu-img create $IMG 1G
+qemu-system-x86_64 -enable-kvm \
+    -append "loglevel=7 root=/dev/sda console=ttyS0" -serial stdio \
+    -m 1G -drive format=raw,file=$IMG \
+    -kernel build/linux/arch/x86/boot/bzImage #-initrd build/initrd/initrd
 rm -f $IMG
