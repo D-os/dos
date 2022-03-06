23723be5611419216865b78720562dccf5fea6e2
Tomasz Sterna <tomasz@sterna.link>
2022-03-06 13:34:40 +0100

    Build initramfs

--- qemu.sh
+++ qemu.sh
@@ -5,5 +5,5 @@ qemu-img create $IMG 1G
 qemu-system-x86_64 -enable-kvm \
     -append "loglevel=7 root=/dev/sda console=ttyS0" -serial stdio \
     -m 1G -drive format=raw,file=$IMG \
-    -kernel build/linux/arch/x86/boot/bzImage #-initrd build/initrd/initrd
+    -kernel build/linux/arch/x86/boot/bzImage -initrd build/initramfs.gz
 rm -f $IMG
