fcb02de438648a188dba99f58d0e11f58952a02c
Tomasz Sterna <tomasz@sterna.link>
2022-03-27 16:16:22 +0200

    Use virtio hdd as /system partition

--- qemu.sh
+++ qemu.sh
@@ -2,9 +2,9 @@ cd $(dirname $0)/out
 IMG=tmp.img
 CMDLINE="loglevel=7 console=ttyS0 androidboot.fstab_suffix=qemu androidboot.first_stage_console=1"
 trap 'rm $IMG' 1 2 3 15
-qemu-img create $IMG 1G
+qemu-img create -f raw $IMG 1G
 qemu-system-x86_64 -enable-kvm \
     -append "$CMDLINE" -serial stdio \
-    -m 1G -drive format=raw,file=$IMG \
+    -m 1G -drive format=raw,file=$IMG,cache=none,aio=threads,if=virtio \
     -kernel build/linux/arch/x86/boot/bzImage -initrd build/initramfs.gz
 rm -f $IMG
