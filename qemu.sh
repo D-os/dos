75a9765ed2904c56f703698dc55f200cf513d44b
Tomasz Sterna <tomasz@sterna.link>
2022-04-02 17:20:00 +0200

    Build systemimg.raw

--- qemu.sh
+++ qemu.sh
@@ -2,11 +2,11 @@ cd $(dirname $0)/out
 IMG=tmp.img
 CMDLINE="loglevel=7 console=ttyS0 androidboot.fstab_suffix=qemu androidboot.first_stage_console=1"
 # BIOS="-bios /usr/share/edk2/ovmf/OVMF_CODE.fd"
+SYSTEM_IMG=build/systemimg.raw
 trap 'rm $IMG' 1 2 3 15
-qemu-img create -f raw $IMG 1G
-mkfs.ext4 $IMG
+qemu-img create -b $SYSTEM_IMG -F raw -f qcow2 $IMG 1G
 qemu-system-x86_64 -enable-kvm $BIOS \
     -append "$CMDLINE" -serial stdio \
-    -m 1G -drive format=raw,file=$IMG,cache=none,aio=threads,if=virtio \
-    -kernel build/linux/arch/x86/boot/bzImage -initrd build/initramfs/initramfs.gz
+    -m 1G -drive format=qcow2,file=$IMG,cache=none,aio=threads,if=virtio \
+    -kernel build/linux/arch/x86/boot/bzImage -initrd build/initramfs.gz
 rm -f $IMG
