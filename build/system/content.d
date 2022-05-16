8c546f89b7cd5954e67ff20ef8713f7ba1ceafc6
Tomasz Sterna <tomasz@sterna.link>
2022-05-16 07:22:34 +0200

    Add xkeyboard-config to system.img

--- build/system/content.d
+++ build/system/content.d
@@ -220,3 +220,5 @@ out/build/system.img: \
   out/build/system/system/bin/weston \
   out/build/system/system/lib/libweston-11.so \
   out/build/system/system/lib/weston/libexec_weston.so \
+  out/build/system/usr/share/X11/xkb \
+  
