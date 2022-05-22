7dac23a93ca4d2e6d0a4e861fce8d446d98f7a8e
Tomasz Sterna <tomasz@sterna.link>
2022-05-22 22:04:10 +0200

    Build Weston gl-renderer

--- build/system/content.d
+++ build/system/content.d
@@ -216,10 +216,15 @@ out/build/system.img: \
   out/build/system/bin/yes \
   out/build/system/bin/zcat \
   out/build/system/bin/ldd \
+  out/build/system/lib/libgbm.so \
+  out/build/system/lib/libglapi.so \
+  out/build/system/lib/libEGL.so \
+  out/build/system/lib/libGLESv2.so \
   out/build/system/system/bin/su \
   out/build/system/system/bin/weston \
   out/build/system/system/lib/libweston-11.so \
   out/build/system/system/lib/weston/libexec_weston.so \
   out/build/system/system/lib/libweston-11/drm-backend.so \
+  out/build/system/system/lib/libweston-11/gl-renderer.so \
   out/build/system/usr/share/X11/xkb \
   
