17b9d1d35cda7fc842507df6d413597e4c3ea44b
Tomasz Sterna <tomasz@sterna.link>
2022-06-13 18:08:56 +0200

    Use virtio mouse and keyboard

--- qemu.sh
+++ qemu.sh
@@ -8,7 +8,8 @@ qemu-img create -b $SYSTEM_IMG -F raw -f qcow2 $IMG 5G
 qemu-system-x86_64 -enable-kvm -machine q35 -cpu host -smp 4 -m 8G \
     -device virtio-vga-gl -display sdl,gl=on \
     -device intel-hda -device hda-duplex \
-    -usb -device usb-mouse -device usb-kbd \
+    -device virtio-mouse -device virtio-keyboard \
+    -rtc clock=host,base=localtime \
     $BIOS -append "$CMDLINE" -serial mon:stdio \
     -drive format=qcow2,file=$IMG,cache=none,aio=threads,if=virtio \
     -kernel build/linux/arch/x86/boot/bzImage -initrd build/initramfs.gz
