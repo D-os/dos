b4b05e2ee1cbbebe9d76290dd950211b3b68cb9b
Tomasz Sterna <tomasz@sterna.link>
2022-06-13 15:33:22 +0200

    Add audio and input devices to QEMU

--- qemu.sh
+++ qemu.sh
@@ -6,7 +6,9 @@ SYSTEM_IMG=system.img
 trap 'rm $IMG' 1 2 3 15
 qemu-img create -b $SYSTEM_IMG -F raw -f qcow2 $IMG 5G
 qemu-system-x86_64 -enable-kvm -machine q35 -cpu host -smp 4 -m 8G \
-    -device virtio-vga-gl -display sdl,gl=on -soundhw hda \
+    -device virtio-vga-gl -display sdl,gl=on \
+    -device intel-hda -device hda-duplex \
+    -usb -device usb-mouse -device usb-kbd \
     $BIOS -append "$CMDLINE" -serial stdio \
     -drive format=qcow2,file=$IMG,cache=none,aio=threads,if=virtio \
     -kernel build/linux/arch/x86/boot/bzImage -initrd build/initramfs.gz
