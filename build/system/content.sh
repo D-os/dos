a4d8832a1fa957bbac54a23e88b37b5c2aaac50d
Tomasz Sterna <tomasz@sterna.link>
2022-06-26 00:29:14 +0200

    Test whether all system image files are installed

--- build/system/content.sh
+++ build/system/content.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -e
+
+# first check whether all files mentioned in content.d are installed
+make -s -f build/system/content.d || exit 1
+
+# mount with fakeroot and run fixes
 mkdir -p out/mnt
 fuse2fs out/system.img out/mnt -o fakeroot,rw || exit 1
 
@@ -12,5 +17,6 @@ mkdir -p out/mnt/users/2000
 chown 2000 out/mnt/users/2000
 chmod 0700 out/mnt/users/2000
 
+# finally sync and unmount
 sync
 fusermount -u -z out/mnt
